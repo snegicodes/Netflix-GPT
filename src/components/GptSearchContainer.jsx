69e57d3c258a1ad87d042378f3488ce04efb4fa6
snegicodes <snegicodes@gmail.com>
2023-09-10 12:31:45 +0530

    fix: Vercel Build Issue

---
+++ src/components/GptSearchContainer.jsx
@@ -0,0 +1,13 @@
+import GptSeachBar from "./GptSearchBar";
+import GptSearchSuggestions from "./GptSearchSuggestions";
+
+const GptSearchContainer = () => {
+  return (
+    <div>
+      <GptSeachBar />
+      <GptSearchSuggestions />
+    </div>
+  );
+};
+
+export default GptSearchContainer;
